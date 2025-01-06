afdb5a7064fed21da65ae9543da62c722820e1fd
LettCosta <leticiacostaaa10@gmail.com>
2025-01-05 23:50:35 -0300

    inicia responsividade pagina inicial

--- app/(home)/pagina_inicial/page.tsx
+++ app/(home)/pagina_inicial/page.tsx
@@ -3,7 +3,7 @@ import Card from "@/components/pagina_inicial/cards/card";
 export default function PaginaInicial(){
     return(
         <div className="w-full flex flex-col gap-10 justify-center">
-            <div className="flex flex-row items-center justify-between">
+            <div className="flex flex-col md:flex-row gap-3 items-center justify-between">
                 <Card/>
                 <Card/>
                 <Card/>
--- components/pagina_inicial/cards/card.tsx
+++ components/pagina_inicial/cards/card.tsx
@@ -1,6 +1,6 @@
 export default function Card(){
     return(
-        <div className="bg-white flex flex-col gap-1 rounded-xl border px-16 py-4 drop-shadow-lg">
+        <div className="bg-white flex flex-col gap-1 rounded-xl border md:px-8 lg:px-12 xl:px-16 px-16 py-4 drop-shadow-lg">
             <h1 className="text-black font-semibold">Saldo Total</h1>
             <h1 className="text-black">R$ 2500,00</h1>
         </div>
