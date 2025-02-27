739228abe457d41d4b1af8ee02e4d3844e3e77d0
LettCosta <leticiacostaaa10@gmail.com>
2025-02-27 00:43:08 -0300

    altera responsividade no card da pagina inicial

--- components/pagina_inicial/cards/card.tsx
+++ components/pagina_inicial/cards/card.tsx
@@ -5,7 +5,7 @@ interface Props {
 
 export default function Card({ text, value }: Props){
     return(
-        <div className="bg-white flex flex-col gap-1 rounded-xl border md:px-8 lg:px-12 xl:px-16 px-16 py-4 drop-shadow-lg">
+        <div className="bg-white flex flex-col gap-1 rounded-xl border w-10/12 md:w-auto md:px-8 lg:px-12 xl:px-16 px-16 py-4 drop-shadow-lg">
             <h1 className="text-black font-semibold">{ text }</h1>
             <h1 className="text-black">R$ { value }</h1>
         </div>
